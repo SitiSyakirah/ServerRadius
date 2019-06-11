4f719a61f7a80f5e08484650edf41feaa634960c
SitiSyakirah <48506122+SitiSyakirah@users.noreply.github.com>
2019-06-11 23:23:56 +0800

    Update radiuscode.rs

--- radiuscode.rs
+++ radiuscode.rs
@@ -1,6 +1,6 @@
 /// RADIUS message types
 #[derive(Clone, PartialEq)]
-fn main() -> radiuscode {
+fn main()-> radiuscode{
 pub enum fn main() -> RadiusCode {
     /// 1 = Access-Request
     AccessRequest,
